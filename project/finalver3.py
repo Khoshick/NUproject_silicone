8efbe76a4741bf96886af30314b8c342b0b4cf87
Khoshick <95194789+Khoshick@users.noreply.github.com>
2021-12-09 13:14:36 -0600

    Update finalver3.py

--- project/finalver3.py
+++ project/finalver3.py
@@ -66,5 +66,6 @@ ANN_model = keras.models.load_model("silicon_ySS.h5")
 input = scaler.transform(input)
 predict=ANN_model.predict(input)
 st.subheader('Silica Percent =')
-st.write(predict)
 st.markdown('Change the process parameters if the above value is greater than 2.5')
+st.write(predict)
+
